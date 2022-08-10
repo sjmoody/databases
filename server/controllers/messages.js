1bd148a9f3d456876544df8251bf02f8ad766cb8
Steven Moody <smoody07@gmail.com>
2022-08-09 19:45:11 -0700

    gets static routes working

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -1,6 +1,16 @@
 var models = require('../models');
 
 module.exports = {
-  get: function (req, res) {}, // a function which handles a get request for all messages
-  post: function (req, res) {} // a function which handles posting a message to the database
+  get: function (req, res) {
+    // models.messages.getAll will reference the get function in the model
+    res.send('You requested Messages');
+    console.log('request made to get messages');
+  }, // a function which handles a get request for all messages
+  // We already know this is for the messages endpoint
+
+  post: function (req, res) {
+    res.send("You posted a message");
+        // models.messages.create will create in the db
+
+  } // a function which handles posting a message to the database
 };
